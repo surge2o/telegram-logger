28fb9473c901b22c10a06585b233d6d2c1b0024c
surge2o <42393248+surge2o@users.noreply.github.com>
2025-08-05 17:09:02 +0100

    Update telegram-logger.js

--- telegram-logger.js
+++ telegram-logger.js
@@ -1,7 +1,7 @@
 
-const express = require 'express';
-const axios = require("axios");
-const cors = require("cors");
+import express from 'express';
+import axios = from 'axios';
+import cors = from 'cors';
 
 const app = express();
 app.use(express.json());
